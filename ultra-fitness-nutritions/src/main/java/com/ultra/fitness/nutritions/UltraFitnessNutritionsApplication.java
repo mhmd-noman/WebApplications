5a5412d2abe859eecbc8a9ea4fc05840829c50ac
Muhammad Nauman <36780231+NomanAbuBakar@users.noreply.github.com>
2021-04-26 01:07:16 +0500

    Adding Project
    Adding Project

---
+++ ultra-fitness-nutritions/src/main/java/com/ultra/fitness/nutritions/UltraFitnessNutritionsApplication.java
@@ -0,0 +1,13 @@
+package com.ultra.fitness.nutritions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraFitnessNutritionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltraFitnessNutritionsApplication.class, args);
+	}
+
+}
---
+++ ultra-fitness-nutritions/src/test/java/com/ultra/fitness/nutritions/UltraFitnessNutritionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultra.fitness.nutritions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraFitnessNutritionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
