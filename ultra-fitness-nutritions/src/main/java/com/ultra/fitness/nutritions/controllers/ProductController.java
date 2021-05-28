4a5e67b31a47e5dc3c68b7234d81a159298bf7a2
Muhammad Nauman <36780231+NomanAbuBakar@users.noreply.github.com>
2021-05-29 02:28:09 +0500

    Getting Products with Thymeleaf and some basic utility methods added
    Product listing integration has been initialized using thymeleaf dynamically and some utility methods are added as well.
    
    INFO: READ-ME file to be used to put aside snippets that can be useful in future.

--- ultra-fitness-nutritions/src/main/java/com/ultra/fitness/nutritions/controllers/ProductController.java
+++ ultra-fitness-nutritions/src/main/java/com/ultra/fitness/nutritions/controllers/ProductController.java
@@ -1,16 +1,40 @@
 package com.ultra.fitness.nutritions.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ultra.fitness.nutritions.service.ProductService;
+import com.ultra.fitness.nutritions.utils.Utilities;
+
+import common.beans.Product;
+import common.request.MainRequestObject;
+import common.response.MainResponseObject;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 	
 	@Autowired
 	private ProductService productService;
+	private MainResponseObject response;
+	private MainRequestObject request;
 
+	@RequestMapping(value="/getProducts", method = RequestMethod.GET)
+	public String getProducts(Model model) {
+		request = new MainRequestObject();
+		request.setProductInfo(new Product());
+		logger.info(logger.isInfoEnabled() ? "Going to get products from Product controller ...": null);
+		response = productService.getProducts(request);
+		logger.info(logger.isInfoEnabled() ? "Response Received: [ " +response.getResponseCode()+ " ][" +response.getResponseDesc()+ "]": null);
+		if (!Utilities.isNullOrEmptyCollection(response.getProducts())) {
+			model.addAttribute("products", response.getProducts());
+		}
+		return "product-list";
+	}
 }
--- ultra-fitness-nutritions/src/main/java/com/ultra/fitness/nutritions/utils/Utilities.java
+++ ultra-fitness-nutritions/src/main/java/com/ultra/fitness/nutritions/utils/Utilities.java
@@ -1,5 +1,94 @@
 package com.ultra.fitness.nutritions.utils;
 
-public class Utilities {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
 
+public class Utilities {
+	public static boolean isNullOrEmptyCollection(final Collection<?> c) {
+	    return c == null || c.isEmpty();
+	}
+
+	public static boolean isNullOrEmptyMap(final Map<?,?> c) {
+	    return c == null || c.isEmpty();
+	}
+	
+	public static boolean isNullOrEmptyString(String s) {
+	    return s == null ||s.isEmpty() || s.trim().length() < 1;
+	}
+	
+	public static boolean validateIfNullOrEmptyString(String str) {
+		if (null == str || str.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean isValidString(String str) {
+		if (null != str && " " != str && str.trim().length() > 1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean validateIfNullOrInvalidInteger(Integer number) {
+		if (null == number || number < 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean validateIfNullOrInvalidDouble(Double number) {
+		if (null == number) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean isValidDate(Date date, String formatForValidation) {
+		SimpleDateFormat sdf = new SimpleDateFormat(formatForValidation);
+		String dateToBeValidated = null; 
+		boolean valid = false;
+		
+		if (null == date) {
+			return false;
+		}
+		
+		dateToBeValidated = date.toString();
+		if (isNullOrEmptyString(dateToBeValidated)) {
+			return false;
+		}
+		
+		try {
+			sdf.parse(dateToBeValidated);
+			// strict mode - check 30 or 31 days, leap year
+			sdf.setLenient(false);
+			valid = true;
+		} catch (ParseException e) {
+			e.printStackTrace();
+			valid = false;
+		}
+		return valid;
+	}
+	
+	public static boolean isValidDateForString(String date, String formatForValidation) {
+		SimpleDateFormat sdf = new SimpleDateFormat(formatForValidation);
+		boolean valid = false;
+		
+		if (isNullOrEmptyString(date)) {
+			return false;
+		}
+		try {
+			sdf.parse(date);
+			// strict mode - check 30 or 31 days, leap year
+			sdf.setLenient(false);
+			valid = true;
+		} catch (ParseException e) {
+			e.printStackTrace();
+			valid = false;
+		}
+		return valid;
+	}
 }
